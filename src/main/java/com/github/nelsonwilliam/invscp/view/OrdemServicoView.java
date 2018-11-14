695efa649f489f41a737dcb1d86aaad8848672f9
larissachyevena <larissac.lopes@hotmail.com>
2018-11-14 16:19:13 -0200

    OrdemServicoView update

--- src/main/java/com/github/nelsonwilliam/invscp/view/OrdemServicoView.java
+++ src/main/java/com/github/nelsonwilliam/invscp/view/OrdemServicoView.java
@@ -1,5 +1,53 @@
 package com.github.nelsonwilliam.invscp.view;
 
-public interface OrdemServicoView {
+import java.awt.event.ActionListener;
+import java.util.List;
 
+import com.github.nelsonwilliam.invscp.model.dto.BemDTO;
+import com.github.nelsonwilliam.invscp.model.dto.OrdemServicoDTO;
+
+public interface OrdemServicoView extends View {
+    // ----------------------------------------
+    // Métodos para notificar ações do usuário.
+    // ----------------------------------------
+
+    void addConfirmarListener(ActionListener listener);
+
+    // -------------------------------------------
+    // Métodos para atualizar os valores exibidos.
+    // -------------------------------------------
+
+    /**
+     * Atualiza todos os valores da janela para exibir os dados da ordem de
+     * serviço passado.
+     */
+    void updateOrdemServico(OrdemServicoDTO sala, final List<BemDTO> bens);
+
+    /**
+     * Exibe uma mensagem de erro com a mensagem especificada. Por exemplo, pode
+     * ser chamado para avisar que algum campo obrigatório foi deixado vazio.
+     */
+    void showError(String message);
+
+    /**
+     * Exibe uma mensagem de que o item foi alterado/adicionado com sucesso.
+     */
+    void showSucesso();
+
+    void showInfo(String message);
+
+    /**
+     * Fecha a View.
+     */
+    void close();
+
+    // ---------------------------------------------
+    // Métodos para obter os valores de formulários.
+    // ---------------------------------------------
+
+    /**
+     * Obtém a ordem de serviço com os valores atualmente exibidos no formulário
+     * que o usuário pode ou não ter alterado.
+     */
+    OrdemServicoDTO getOrdemServico();
 }
