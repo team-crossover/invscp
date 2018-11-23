9413e561d1f92b621c0d1fbd7e11b720e7ae5d43
larissachyevena <larissac.lopes@hotmail.com>
2018-11-23 19:11:25 -0200

    View e SwingView para Movimentacoes adicionadas

--- src/main/java/com/github/nelsonwilliam/invscp/view/MovimentacoesView.java
+++ src/main/java/com/github/nelsonwilliam/invscp/view/MovimentacoesView.java
@@ -1,5 +1,53 @@
 package com.github.nelsonwilliam.invscp.view;
 
-public interface MovimentacoesView {
+import java.awt.event.ActionListener;
+import java.util.List;
+import java.util.function.Consumer;
+
+import com.github.nelsonwilliam.invscp.model.dto.MovimentacaoDTO;
+
+public interface MovimentacoesView extends View {
+
+    // ----------------------------------------
+    // Métodos para notificar ações do usuário.
+    // ----------------------------------------
+
+    void addAdicionarMovimentacaoListener(ActionListener listener);
+
+    void addGerarRelatorioListener(ActionListener listener);
+
+    void addDeletarMovimentacoesListener(ActionListener listener);
+
+    /**
+     * Adiciona o Listener para quando o usuário informar que quer alterar uma
+     * movimentação. Espera-se que quando este evento for invocado, haja
+     * exatamente UMA movimentação selecionado (obtível através do método
+     * getSelectedMovimentacoesIds.
+     */
+    void addAlterarMovimentacaoListener(ActionListener listener);
+
+    // -------------------------------------------
+    // Métodos para atualizar os valores exibidos.
+    // -------------------------------------------
+
+    void updateMovimentacoes(List<MovimentacaoDTO> movimentacao);
+
+    void showError(String message);
+
+    void showSucesso(String message);
+
+    void showInfo(String message);
+
+    void showConfirmacao(String message, Consumer<Boolean> responseCallback);
+
+    // ---------------------------------------------
+    // Métodos para obter os valores de formulários.
+    // ---------------------------------------------
+
+    /**
+     * Obtém uma lista com os IDs de todos as movimentações atualmente
+     * selecionados na tabela.
+     */
+    List<Integer> getSelectedMovimentacoesIds();
 
 }
--- src/main/java/com/github/nelsonwilliam/invscp/view/swing/MovimentacoesSwingView.java
+++ src/main/java/com/github/nelsonwilliam/invscp/view/swing/MovimentacoesSwingView.java
@@ -1,7 +1,246 @@
 package com.github.nelsonwilliam.invscp.view.swing;
 
-public class MovimentacoesSwingView {
+import java.awt.Color;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.Point;
+import java.awt.event.ActionListener;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
 
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JPopupMenu;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.ListSelectionModel;
+import javax.swing.SwingUtilities;
+import javax.swing.event.PopupMenuEvent;
+import javax.swing.event.PopupMenuListener;
+import javax.swing.table.DefaultTableModel;
+
+import com.github.nelsonwilliam.invscp.model.dto.BemDTO;
+import com.github.nelsonwilliam.invscp.model.dto.MovimentacaoDTO;
+import com.github.nelsonwilliam.invscp.model.dto.SalaDTO;
+import com.github.nelsonwilliam.invscp.model.enums.EtapaMovEnum;
+import com.github.nelsonwilliam.invscp.view.MovimentacoesView;
+
+public class MovimentacoesSwingView extends JPanel
+        implements MovimentacoesView {
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = -9210173032768696552L;
+
+    private JTable table;
+    private JButton btnAdicionar;
+    private JButton btnDeletar;
+    private JPopupMenu popupMenu; // Popup exibido ao clicar com o botão direito
+                                  // em um item da
+                                  // tabela
+    private JMenuItem popupItemAlterar;
+    private JMenuItem popupItemRelatorio;
+
+    public MovimentacoesSwingView() {
+        initialize();
+    }
+
+    private void initialize() {
+        setBounds(0, 0, 850, 700);
+        final GridBagLayout gridBagLayout = new GridBagLayout();
+        gridBagLayout.columnWidths = new int[] { 0, 199, 249, 0 };
+        gridBagLayout.rowHeights = new int[] { 422, 25, 0 };
+        gridBagLayout.columnWeights = new double[] { 0.0, 1.0, 1.0, 0.0 };
+        gridBagLayout.rowWeights = new double[] { 1.0, 0.0, Double.MIN_VALUE };
+        setLayout(gridBagLayout);
+
+        popupItemAlterar = new JMenuItem("Alterar");
+        popupItemRelatorio = new JMenuItem("Gerar relatório...");
+
+        popupMenu = new JPopupMenu();
+        popupMenu.addPopupMenuListener(new PopupMenuListener() {
+            @Override
+            public void popupMenuWillBecomeVisible(final PopupMenuEvent e) {
+                // Garante que ao clicar com o botão direito em um item para
+                // exibir o menu, o
+                // o único item selecionado da tabela será o item clicado.
+                SwingUtilities.invokeLater(() -> {
+                    final int rowAtPoint = table.rowAtPoint(SwingUtilities
+                            .convertPoint(popupMenu, new Point(0, 0), table));
+                    if (rowAtPoint > -1) {
+                        table.setRowSelectionInterval(rowAtPoint, rowAtPoint);
+                    }
+                });
+            }
+
+            @Override
+            public void popupMenuWillBecomeInvisible(final PopupMenuEvent e) {
+            }
+
+            @Override
+            public void popupMenuCanceled(final PopupMenuEvent e) {
+            }
+        });
+        popupMenu.add(popupItemAlterar);
+        popupMenu.addSeparator();
+        popupMenu.add(popupItemRelatorio);
+
+        final JScrollPane scrollPane = new JScrollPane();
+        final GridBagConstraints gbc_scrollPane = new GridBagConstraints();
+        gbc_scrollPane.fill = GridBagConstraints.BOTH;
+        gbc_scrollPane.insets = new Insets(0, 0, 5, 0);
+        gbc_scrollPane.gridwidth = 4;
+        gbc_scrollPane.gridx = 0;
+        gbc_scrollPane.gridy = 0;
+        add(scrollPane, gbc_scrollPane);
+
+        table = new JTable();
+        table.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
+        table.setModel(new DefaultTableModel(new Object[][] {},
+                new String[] { "ID", "Número de guia de transporte", "Etapa",
+                        "Bem", "Sala de origem", "Sala de destino" }) {
+            private static final long serialVersionUID = -5079273255233169992L;
+            Class<?>[] columnTypes = new Class[] { Integer.class, String.class,
+                    EtapaMovEnum.class, BemDTO.class, SalaDTO.class,
+                    SalaDTO.class };
+
+            @Override
+            public Class<?> getColumnClass(final int columnIndex) {
+                return columnTypes[columnIndex];
+            }
+
+            @Override
+            public boolean isCellEditable(final int row, final int column) {
+                return false;
+            }
+        });
+        table.getColumnModel().getColumn(0).setMinWidth(75);
+        table.getColumnModel().getColumn(0).setMaxWidth(75);
+        table.getColumnModel().getColumn(1).setMinWidth(100);
+        table.getColumnModel().getColumn(1).setMaxWidth(100);
+        table.getColumnModel().getColumn(2).setMinWidth(200);
+        table.getColumnModel().getColumn(3).setMinWidth(200);
+        table.getColumnModel().getColumn(4).setMinWidth(200);
+        table.setAutoCreateRowSorter(true);
+        scrollPane.setBorder(BorderFactory.createLineBorder(Color.gray, 1));
+        scrollPane.setViewportView(table);
+        scrollPane.addComponentListener(new ComponentAdapter() {
+            @Override
+            public void componentResized(final ComponentEvent e) {
+                if (table.getPreferredSize().width < scrollPane.getWidth()) {
+                    table.setAutoResizeMode(JTable.AUTO_RESIZE_ALL_COLUMNS);
+                } else {
+                    table.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
+                }
+            }
+        });
+        table.setComponentPopupMenu(popupMenu);
+
+        btnAdicionar = new JButton("Adicionar novo");
+        final GridBagConstraints gbc_btnAdicionar = new GridBagConstraints();
+        gbc_btnAdicionar.anchor = GridBagConstraints.SOUTHWEST;
+        gbc_btnAdicionar.insets = new Insets(0, 0, 0, 5);
+        gbc_btnAdicionar.gridx = 1;
+        gbc_btnAdicionar.gridy = 1;
+        add(btnAdicionar, gbc_btnAdicionar);
+
+        btnDeletar = new JButton("Deletar selecionado(s)");
+        final GridBagConstraints gbc_btnDeletar = new GridBagConstraints();
+        gbc_btnDeletar.insets = new Insets(0, 0, 0, 5);
+        gbc_btnDeletar.anchor = GridBagConstraints.SOUTHEAST;
+        gbc_btnDeletar.gridx = 2;
+        gbc_btnDeletar.gridy = 1;
+        add(btnDeletar, gbc_btnDeletar);
+    }
+
+    @Override
+    public void addAdicionarMovimentacaoListener(
+            final ActionListener listener) {
+        btnAdicionar.addActionListener(listener);
+    }
+
+    @Override
+    public void addDeletarMovimentacoesListener(final ActionListener listener) {
+        btnDeletar.addActionListener(listener);
+    }
+
+    @Override
+    public void addGerarRelatorioListener(final ActionListener listener) {
+        popupItemRelatorio.addActionListener(listener);
+    }
+
+    @Override
+    public void addAlterarMovimentacaoListener(final ActionListener listener) {
+        popupItemAlterar.addActionListener(listener);
+    }
+
+    @Override
+    public void updateMovimentacoes(final List<MovimentacaoDTO> movimentacao) {
+        final DefaultTableModel tableModel = (DefaultTableModel) table
+                .getModel();
+        tableModel.setNumRows(0);
+        for (final MovimentacaoDTO m : movimentacao) {
+            tableModel.addRow(new Object[] { m.getId(),
+                    m.getNumGuiaTransporte(), m.getEtapa(), m.getBem(),
+                    m.getSalaDestino() == null ? "Nenhum" : m.getSalaDestino(),
+                    m.getSalaOrigem() == null ? "Nenhum"
+                            : m.getSalaOrigem().getNome() });
+        }
+
+        revalidate();
+        repaint();
+    }
+
+    @Override
+    public void showError(final String message) {
+        final String titulo = "Erro";
+        final String messageCompleta = "Erro: " + message;
+        JOptionPane.showMessageDialog(this, messageCompleta, titulo,
+                JOptionPane.ERROR_MESSAGE);
+    }
+
+    @Override
+    public void showSucesso(final String message) {
+        final String titulo = "Sucesso";
+        final String messageCompleta = message;
+        JOptionPane.showMessageDialog(this, messageCompleta, titulo,
+                JOptionPane.INFORMATION_MESSAGE);
+    }
+
+    @Override
+    public void showConfirmacao(final String message,
+            final Consumer<Boolean> responseCallback) {
+        final String titulo = "Confirmação";
+        final int resposta = JOptionPane.showConfirmDialog(this, message,
+                titulo, JOptionPane.YES_NO_OPTION,
+                JOptionPane.QUESTION_MESSAGE);
+        responseCallback.accept(resposta == JOptionPane.YES_OPTION);
+    }
+
+    @Override
+    public void showInfo(final String message) {
+        final String titulo = "Informação";
+        JOptionPane.showMessageDialog(this, message, titulo,
+                JOptionPane.INFORMATION_MESSAGE);
+    }
+
+    @Override
+    public List<Integer> getSelectedMovimentacoesIds() {
+        final List<Integer> selectedMovimentacoes = new ArrayList<Integer>();
+        final int[] selectedRows = table.getSelectedRows();
+        for (int i = 0; i < selectedRows.length; i++) {
+            final int row = table.convertRowIndexToModel(selectedRows[i]);
+            final Integer id = (Integer) table.getModel().getValueAt(row, 0);
+            selectedMovimentacoes.add(id);
+        }
+        return selectedMovimentacoes;
+    }
 }
-
-//
