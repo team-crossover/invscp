84a6c60d99ddf5dc45dbd58068e6f44b1e0207d8
larissachyevena <larissac.lopes@hotmail.com>
2018-11-23 16:39:56 -0200

    EventosMovimentacaoView

---
+++ src/main/java/com/github/nelsonwilliam/invscp/view/EventosMovimentacaoView.java
@@ -0,0 +1,55 @@
+package com.github.nelsonwilliam.invscp.view;
+
+import java.awt.event.ActionListener;
+import java.util.List;
+import java.util.function.Consumer;
+
+import com.github.nelsonwilliam.invscp.model.dto.EventoMovimentacaoDTO;
+import com.github.nelsonwilliam.invscp.model.dto.MovimentacaoDTO;
+
+public interface EventosMovimentacaoView extends View {
+
+    // ----------------------------------------
+    // Métodos para notificar ações do usuário.
+    // ----------------------------------------
+
+    void addAdicionarEventoMovimentacaoListener(ActionListener listener);
+
+    // void addDeletarEventoMovimentacaoListener(ActionListener listener);
+
+    /**
+     * Adiciona o Listener para quando o usuário informar que quer alterar um
+     * evento de movimentação. Espera-se que quando este evento for invocado,
+     * haja exatamente UMA ordem de serviço selecionado (obtível através do
+     * método getSelectedEventosMovimentacaoIds.
+     */
+    void addAlterarEventoMovimentacaoListener(ActionListener listener);
+
+    void addConcluirEventoMovimentacaoListener(ActionListener listener);
+
+    // -------------------------------------------
+    // Métodos para atualizar os valores exibidos.
+    // -------------------------------------------
+
+    void updateEventosMovimentacao(MovimentacaoDTO movimentacao,
+            List<EventoMovimentacaoDTO> eventoMovimentacao);
+
+    void showError(String message);
+
+    void showSucesso(String message);
+
+    void showInfo(String message);
+
+    void showConfirmacao(String message, Consumer<Boolean> responseCallback);
+
+    // ---------------------------------------------
+    // Métodos para obter os valores de formulários.
+    // ---------------------------------------------
+
+    /**
+     * Obtém uma lista com os IDs de todos os eventos de movimentação atualmente
+     * selecionados na tabela.
+     */
+    List<Integer> getSelectedEventosMovimentacaoIds();
+
+}
