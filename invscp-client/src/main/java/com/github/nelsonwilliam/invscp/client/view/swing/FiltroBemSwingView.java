1741e2b80ccf0fc7893996065c76076c8006c279
larissachyevena <larissac.lopes@hotmail.com>
2018-11-26 11:23:43 -0200

    FiltroBemSwingView update

--- invscp-client/src/main/java/com/github/nelsonwilliam/invscp/client/view/swing/FiltroBemSwingView.java
+++ invscp-client/src/main/java/com/github/nelsonwilliam/invscp/client/view/swing/FiltroBemSwingView.java
@@ -1,24 +1,45 @@
 package com.github.nelsonwilliam.invscp.client.view.swing;
 
+import java.awt.Color;
+import java.awt.Component;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
 import java.awt.event.ActionListener;
 import java.util.List;
 
+import javax.swing.BorderFactory;
+import javax.swing.DefaultListCellRenderer;
+import javax.swing.DefaultListModel;
 import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JScrollPane;
+import javax.swing.JTextField;
+import javax.swing.ListCellRenderer;
 
 import com.github.nelsonwilliam.invscp.client.view.FiltroBemView;
 import com.github.nelsonwilliam.invscp.shared.model.dto.DepartamentoDTO;
 import com.github.nelsonwilliam.invscp.shared.model.dto.FiltroBemDTO;
+import com.github.nelsonwilliam.invscp.shared.model.enums.BemSituacaoEnum;
 
-public class FiltroBemSwingView  extends JDialog implements FiltroBemView {
+public class FiltroBemSwingView extends JDialog implements FiltroBemView {
 
     private static final long serialVersionUID = -276032841299039698L;
 
+    private Integer idDepartamento;
+
     private JButton btnConfirmar;
+    private JTextField fieldDescricao;
+    private JLabel lblNmeroDeTombamento;
+    private JTextField fieldNumeroTombamento;
+    private JLabel lblSituacao;
+    private JLabel labelSituacao;
+    private JList<DepartamentoDTO> listDepartamentos;
+    private JScrollPane scrollDepartamentos;
+    private JLabel lblDepartamento;
 
     public FiltroBemSwingView(final JFrame owner) {
         super(owner, "Filtrar bens", ModalityType.APPLICATION_MODAL);
@@ -26,24 +47,117 @@ public class FiltroBemSwingView  extends JDialog implements FiltroBemView {
     }
 
     private void initialize() {
-        setBounds(0, 0, 300, 200);
+        setBounds(0, 0, 400, 289);
         setLocationRelativeTo(getOwner());
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 
         final GridBagLayout gridBagLayout = new GridBagLayout();
-        gridBagLayout.columnWidths = new int[] { 0, 0, 0, 0 };
-        gridBagLayout.rowHeights = new int[] { 0, 0, 0, 0 };
-        gridBagLayout.columnWeights =
-                new double[] { 0.0, 1.0, 0.0, Double.MIN_VALUE };
-        gridBagLayout.rowWeights =
-                new double[] { 0.0, 1.0, 0.0, Double.MIN_VALUE };
+        gridBagLayout.columnWidths = new int[] { 149, 205, 11, 0 };
+        gridBagLayout.rowHeights = new int[] { 0, 0, 0, 0, 84, 0, 0, 0 };
+        gridBagLayout.columnWeights = new double[] { 0.0, 1.0, 0.0,
+                Double.MIN_VALUE };
+        gridBagLayout.rowWeights = new double[] { 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+                0.0, Double.MIN_VALUE };
         getContentPane().setLayout(gridBagLayout);
+
+        final JLabel lblDescricao = new JLabel("Descrição:");
+        final GridBagConstraints gbc_lblDescricao = new GridBagConstraints();
+        gbc_lblDescricao.anchor = GridBagConstraints.EAST;
+        gbc_lblDescricao.insets = new Insets(0, 0, 5, 5);
+        gbc_lblDescricao.gridx = 0;
+        gbc_lblDescricao.gridy = 1;
+        getContentPane().add(lblDescricao, gbc_lblDescricao);
+
+        fieldDescricao = new JTextField();
+        final GridBagConstraints gbc_txtfldDescricao = new GridBagConstraints();
+        gbc_txtfldDescricao.insets = new Insets(0, 0, 5, 5);
+        gbc_txtfldDescricao.fill = GridBagConstraints.HORIZONTAL;
+        gbc_txtfldDescricao.gridx = 1;
+        gbc_txtfldDescricao.gridy = 1;
+        getContentPane().add(fieldDescricao, gbc_txtfldDescricao);
+        fieldDescricao.setColumns(10);
+
+        lblSituacao = new JLabel("Situação:");
+        final GridBagConstraints gbc_lblSituacao = new GridBagConstraints();
+        gbc_lblSituacao.anchor = GridBagConstraints.EAST;
+        gbc_lblSituacao.insets = new Insets(0, 0, 5, 5);
+        gbc_lblSituacao.gridx = 0;
+        gbc_lblSituacao.gridy = 2;
+        getContentPane().add(lblSituacao, gbc_lblSituacao);
+
+        labelSituacao = new JLabel((String) null);
+        final GridBagConstraints gbc_label = new GridBagConstraints();
+        gbc_label.anchor = GridBagConstraints.WEST;
+        gbc_label.insets = new Insets(0, 0, 5, 5);
+        gbc_label.gridx = 1;
+        gbc_label.gridy = 2;
+        getContentPane().add(labelSituacao, gbc_label);
+
+        lblNmeroDeTombamento = new JLabel("Número de tombamento:");
+        final GridBagConstraints gbc_lblNmeroDeTombamento = new GridBagConstraints();
+        gbc_lblNmeroDeTombamento.anchor = GridBagConstraints.EAST;
+        gbc_lblNmeroDeTombamento.insets = new Insets(0, 0, 5, 5);
+        gbc_lblNmeroDeTombamento.gridx = 0;
+        gbc_lblNmeroDeTombamento.gridy = 3;
+        getContentPane().add(lblNmeroDeTombamento, gbc_lblNmeroDeTombamento);
+
+        fieldNumeroTombamento = new JTextField();
+        fieldNumeroTombamento.setColumns(10);
+        final GridBagConstraints gbc_fieldNumeroTombamento = new GridBagConstraints();
+        gbc_fieldNumeroTombamento.insets = new Insets(0, 0, 5, 5);
+        gbc_fieldNumeroTombamento.fill = GridBagConstraints.HORIZONTAL;
+        gbc_fieldNumeroTombamento.gridx = 1;
+        gbc_fieldNumeroTombamento.gridy = 3;
+        getContentPane().add(fieldNumeroTombamento, gbc_fieldNumeroTombamento);
+
+        final ListCellRenderer<? super DepartamentoDTO> deptListRenderer = new DefaultListCellRenderer() {
+
+            private static final long serialVersionUID = -6531942280813288434L;
+
+            @Override
+            public Component getListCellRendererComponent(final JList<?> list,
+                    final Object value, final int index,
+                    final boolean isSelected, final boolean cellHasFocus) {
+                if (value == null) {
+                    return super.getListCellRendererComponent(list, "Nenhum",
+                            index, isSelected, cellHasFocus);
+                } else {
+                    final String nome = ((DepartamentoDTO) value).getNome();
+                    return super.getListCellRendererComponent(list, nome, index,
+                            isSelected, cellHasFocus);
+                }
+            }
+        };
+
+        lblDepartamento = new JLabel("Departamento:");
+        final GridBagConstraints gbc_lblDepartamento = new GridBagConstraints();
+        gbc_lblDepartamento.anchor = GridBagConstraints.EAST;
+        gbc_lblDepartamento.insets = new Insets(0, 0, 5, 5);
+        gbc_lblDepartamento.gridx = 0;
+        gbc_lblDepartamento.gridy = 4;
+        getContentPane().add(lblDepartamento, gbc_lblDepartamento);
+
+        scrollDepartamentos = new JScrollPane();
+        final GridBagConstraints gbc_scrollPaneDepts = new GridBagConstraints();
+        gbc_scrollPaneDepts.fill = GridBagConstraints.BOTH;
+        gbc_scrollPaneDepts.insets = new Insets(0, 0, 5, 5);
+        gbc_scrollPaneDepts.gridx = 1;
+        gbc_scrollPaneDepts.gridy = 4;
+        getContentPane().add(scrollDepartamentos, gbc_scrollPaneDepts);
+
+        listDepartamentos = new JList<DepartamentoDTO>();
+        listDepartamentos.setModel(new DefaultListModel<DepartamentoDTO>());
+        listDepartamentos.setCellRenderer(deptListRenderer);
+        scrollDepartamentos
+                .setBorder(BorderFactory.createLineBorder(Color.gray, 1));
+        scrollDepartamentos.setViewportView(listDepartamentos);
 
         btnConfirmar = new JButton("Confirmar");
         final GridBagConstraints gbc_btnConfirmar = new GridBagConstraints();
+        gbc_btnConfirmar.anchor = GridBagConstraints.WEST;
         gbc_btnConfirmar.insets = new Insets(0, 0, 0, 5);
         gbc_btnConfirmar.gridx = 1;
-        gbc_btnConfirmar.gridy = 2;
+        gbc_btnConfirmar.gridy = 6;
         getContentPane().add(btnConfirmar, gbc_btnConfirmar);
     }
 
@@ -66,13 +180,55 @@ public class FiltroBemSwingView  extends JDialog implements FiltroBemView {
     @Override
     public void updateFiltro(final FiltroBemDTO newFiltro,
             final List<DepartamentoDTO> newDepartamentos) {
-        // TODO Auto-generated method stub
+
+        if (newFiltro == null) {
+            throw new NullPointerException();
+        }
+
+        idDepartamento = newFiltro.getIdDepartamento();
+
+        fieldDescricao.setText(newFiltro.getDescricao());
+        if (newFiltro.getNumeroTombamento() != null) {
+            fieldNumeroTombamento
+                    .setText(newFiltro.getNumeroTombamento().toString());
+        }
+        if (newFiltro.getSituacao() != null) {
+            labelSituacao.setText(newFiltro.getSituacao().getTexto());
+        }
+
+        // final DepartamentoDTO dept = newFiltro.getIdDepartamento();
+        // final DefaultListModel<DepartamentoDTO> listDeptsModel =
+        // (DefaultListModel<DepartamentoDTO>) listDepartamentos
+        // .getModel();
+        // listDeptsModel.clear();
+        // if (dept == null) {
+        // listDeptsModel.add(0, null);
+        // listDepartamentos.setSelectedIndex(0);
+        // } else {
+        // listDeptsModel.add(0, dept);
+        // listDepartamentos.setSelectedIndex(0);
+        // }
+        // if (newDepartamentos != null) {
+        // for (final DepartamentoDTO d : newDepartamentos) {
+        // if (dept == null || !d.getId().equals(dept.getId())) {
+        // listDeptsModel.addElement(d);
+        // }
+        // }
+        // }
+
+        revalidate();
+        repaint();
     }
 
     @Override
     public FiltroBemDTO getFiltro() {
         final FiltroBemDTO filtro = new FiltroBemDTO();
-        // TODO Auto-generated method stub
+        filtro.setDescricao(fieldDescricao.getText());
+        filtro.setIdDepartamento(idDepartamento);
+        filtro.setNumeroTombamento(
+                Integer.parseInt(fieldNumeroTombamento.getText()));
+        filtro.setSituacao(
+                (BemSituacaoEnum.valueOfTexto(labelSituacao.getText())));
         return filtro;
     }
 
---
+++ target/classes/sql/createDatabase.sql
@@ -0,0 +1,232 @@
+/* Cria as tabelas e valores iniciais do InvSCP */
+
+/* Deleta as tabelas anteriores */
+
+DROP SCHEMA public CASCADE;
+CREATE SCHEMA public;
+ALTER SCHEMA public OWNER TO "invscpAdmin";
+
+/* Cria as novas tabelas */
+
+CREATE TABLE localizacao (
+    id serial PRIMARY KEY,
+    nome character varying(255) NOT NULL,
+    endereco character varying(255) NOT NULL,
+    cep character varying(8) NOT NULL,
+    cidade character varying(255) NOT NULL,
+    uf character varying(2) NOT NULL
+);
+
+CREATE TABLE predio (
+    id serial PRIMARY KEY,
+    nome character varying(255) NOT NULL
+);
+
+CREATE TABLE sala (
+    id serial PRIMARY KEY,
+    nome character varying(255) NOT NULL,
+    tipo character varying(255) NOT NULL
+);
+
+CREATE TABLE funcionario (
+    id serial PRIMARY KEY,
+    login character varying(255) UNIQUE NOT NULL,
+    senha character varying(255) NOT NULL,
+    nome character varying(255) NOT NULL,
+    cpf character varying(11) NOT NULL,
+    email character varying(255) NOT NULL
+);
+
+CREATE TABLE departamento (
+    id serial PRIMARY KEY,
+    nome character varying(255) NOT NULL,
+    de_patrimonio boolean NOT NULL
+);
+
+CREATE TABLE bem (
+    id serial PRIMARY KEY,
+    descricao character varying(255) NOT NULL,
+    numero_tombamento bigint UNIQUE NOT NULL,
+    data_cadastro date NOT NULL,
+    data_aquisicao date NOT NULL,
+    numero_nota_fiscal character varying(30) NOT NULL,
+    especificacao character varying(255) NOT NULL,
+    garantia date NOT NULL,
+    marca character varying(255) NOT NULL,
+    valor_compra numeric(12, 2) NOT NULL,
+    situacao character varying(255) NOT NULL
+);
+
+CREATE TABLE ordem_servico (
+    id serial PRIMARY KEY,
+    data_cadastro date NOT NULL,
+    data_conclusao date,
+    valor numeric(12, 2) NOT NULL,
+    situacao character varying(255) NOT NULL
+);
+
+CREATE TABLE baixa (
+    id serial PRIMARY KEY,
+    data date NOT NULL,
+    motivo character varying(255) NOT NULL,
+    observacoes character varying(255)
+);
+
+CREATE TABLE grupo_material (
+    id serial PRIMARY KEY,
+    nome character varying(255) NOT NULL,
+    vida_util integer NOT NULL,
+    depreciacao numeric(6, 5) NOT NULL
+);
+
+CREATE TABLE movimentacao ( 
+   id serial PRIMARY KEY,
+   etapa character varying(255) NOT NULL,
+   num_guia_transporte character varying(32)
+);
+
+CREATE TABLE evento_movimentacao(
+   id serial PRIMARY KEY,
+   tipo character varying(255) NOT NULL,
+   data date NOT NULL,
+   justificativa character varying(255)
+);
+
+/* Adiciona as chaves estrangeiras */
+
+ALTER TABLE predio
+    ADD COLUMN id_localizacao integer references localizacao(id);
+
+ALTER TABLE sala
+    ADD COLUMN id_predio integer references predio(id),
+    ADD COLUMN id_departamento integer references departamento(id);
+    
+ALTER TABLE funcionario
+    ADD COLUMN id_departamento integer references departamento(id);
+
+ALTER TABLE departamento
+    ADD COLUMN id_chefe integer references funcionario(id),
+    ADD COLUMN id_chefe_substituto integer references funcionario(id);
+
+ALTER TABLE ordem_servico
+    ADD COLUMN id_funcionario integer references funcionario(id),
+    ADD COLUMN id_bem integer references bem(id);
+
+ALTER TABLE baixa
+    ADD COLUMN id_funcionario integer references funcionario(id),
+    ADD COLUMN id_bem integer references bem(id);
+
+ALTER TABLE bem
+    ADD COLUMN id_sala integer references sala(id),
+    ADD COLUMN id_departamento integer references departamento(id),
+    ADD COLUMN id_grupo_material integer references grupo_material(id);
+
+ALTER TABLE movimentacao
+    ADD COLUMN id_bem integer references bem(id),
+    ADD COLUMN id_sala_origem integer references sala(id),
+    ADD COLUMN id_sala_destino integer references sala(id);
+
+ALTER TABLE evento_movimentacao
+    ADD COLUMN id_movimentacao integer references movimentacao(id),
+    ADD COLUMN id_funcionario integer references funcionario(id);
+
+/* Adiciona os valores iniciais */
+
+DO $$
+DECLARE chefeId integer;
+DECLARE departamentoId integer;
+DECLARE localizacaoId integer;
+DECLARE predioId integer;
+DECLARE salaId integer;
+BEGIN
+    INSERT INTO funcionario (login, senha, nome, cpf, email)
+        VALUES ('admin', 'admin', 'Administrador', '00000000000', 'admin@invscp.com') 
+        RETURNING id INTO chefeId;
+
+    INSERT INTO departamento (nome, de_patrimonio)
+        VALUES ('Departamento de Patrimônio', TRUE)
+        RETURNING id INTO departamentoId;
+
+    INSERT INTO localizacao (nome, endereco, cep, cidade, uf)
+        VALUES ('Localização do Depósito', 'Sem endereço', '00000000', 'Acrelândia', 'AC')
+        RETURNING id INTO localizacaoId;
+
+    INSERT INTO predio (nome)
+        VALUES ('Prédio do Depósito')
+        RETURNING id INTO predioId;
+
+    INSERT INTO sala (nome, tipo)
+        VALUES ('Depósito', 'DEPOSITO')
+        RETURNING id INTO salaId;
+
+    UPDATE predio
+        SET id_localizacao = localizacaoId
+        WHERE id = predioId;
+
+    UPDATE sala
+        SET id_predio = predioId, id_departamento = departamentoId
+        WHERE id = salaId;
+
+    UPDATE funcionario
+        SET id_departamento = departamentoId
+        WHERE id = chefeId;
+
+    UPDATE departamento
+        SET id_chefe = chefeId
+        WHERE id = departamentoId;
+END $$;
+
+INSERT INTO grupo_material (nome, vida_util, depreciacao)
+	VALUES ('Aeronaves e embarcações', 25, 0.05);
+
+INSERT INTO grupo_material (nome, vida_util, depreciacao)
+	VALUES ('Aparelhos de medição, equipamentos de comunicação e ferramentas', 10, 0.1);
+
+INSERT INTO grupo_material (nome, vida_util, depreciacao)
+	VALUES ('Máquinas e equipamentos industriais', 10, 0.1);
+
+INSERT INTO grupo_material (nome, vida_util, depreciacao)
+	VALUES ('Máquinas e equipamentos de produção de energia', 10, 0.1);
+
+INSERT INTO grupo_material (nome, vida_util, depreciacao)
+	VALUES ('Computadores e equipamentos de informática', 5, 0.2);
+
+INSERT INTO grupo_material (nome, vida_util, depreciacao)
+	VALUES ('Tratores leves, pesados e máquinas agrícolas', 10, 0.2);
+
+INSERT INTO grupo_material (nome, vida_util, depreciacao)
+	VALUES ('Móveis', 10, 0.1);
+
+INSERT INTO grupo_material (nome, vida_util, depreciacao)
+	VALUES ('Veículos leves e pesados em geral', 15, 0.2);
+
+/* Adiciona as restrições de not-null que não existiam antes para permitir a inserção dos valores iniciais */
+
+ALTER TABLE predio
+    ALTER COLUMN id_localizacao SET NOT NULL;
+
+ALTER TABLE sala
+    ALTER COLUMN id_predio SET NOT NULL,
+    ALTER COLUMN id_departamento SET NOT NULL;
+
+ALTER TABLE ordem_servico
+    ALTER COLUMN id_funcionario SET NOT NULL,    
+    ALTER COLUMN id_bem SET NOT NULL;
+
+ALTER TABLE baixa
+    ALTER COLUMN id_funcionario SET NOT NULL,    
+    ALTER COLUMN id_bem SET NOT NULL;
+
+ALTER TABLE bem
+    ALTER COLUMN id_sala SET NOT NULL,    
+    ALTER COLUMN id_departamento SET NOT NULL,
+    ALTER COLUMN id_grupo_material SET NOT NULL;
+
+ALTER TABLE movimentacao
+    ALTER COLUMN id_bem SET NOT NULL, 
+    ALTER COLUMN id_sala_origem SET NOT NULL, 
+    ALTER COLUMN id_sala_destino SET NOT NULL;
+
+ALTER TABLE evento_movimentacao
+    ALTER COLUMN id_movimentacao SET NOT NULL, 
+    ALTER COLUMN id_funcionario SET NOT NULL;
